f543fad52163c3b3b8db43566a34d19919bea12e
PetrMKO <74903849+PetrMKO@users.noreply.github.com>
2022-10-29 11:01:41 +0300

    Broke usersTable Layout

--- src/components/admin/users/Users.tsx
+++ src/components/admin/users/Users.tsx
@@ -108,7 +108,7 @@ const Users: FC = () => {
                 selected: false,
                 check: null,
                 user: getFullName(item.firstname, item.middlename, item.lastname),
-                login: item.username,
+                login: getFullName(item.firstname, item.middlename, item.lastname),
                 provider: item.provider,
                 role: t(`table.roles.${item.role}`),
                 university: item.organization,
