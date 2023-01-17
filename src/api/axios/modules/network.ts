2b78dd77fd3e153fee0f710dbe5c4fb8d2ee5d03
sem_wett <90400355+semwett0301@users.noreply.github.com>
2023-01-17 04:40:48 +0300

    AAAAAAA I understand no more
    I'm going to sleep u know, fuck the async

--- src/api/axios/modules/network.ts
+++ src/api/axios/modules/network.ts
@@ -1,15 +1,19 @@
-import {AxiosInstance, AxiosResponse} from 'axios';
+import { AxiosInstance, AxiosResponse } from 'axios'
 
 export type IpInfo = {
-  ip: string,
-  country: string,
+  ip: string
+  country: string
   city: string
 }
+
 export interface INetworkAxios {
-  getIpInfo: () => Promise<AxiosResponse<IpInfo>>,
+  getIpInfo: () => Promise<AxiosResponse<IpInfo>>
   checkPing: () => Promise<AxiosResponse<void>>
-  getRX: (onDownloadProgress?: (event: ProgressEvent) => void) => Promise<AxiosResponse<void>>
-  getTX: (onDownloadProgress?: (event: ProgressEvent) => void) => Promise<AxiosResponse<void>>
+  getRX: (onUploadProgress?: (event: ProgressEvent) => void) => Promise<AxiosResponse<void>>
+  getTX: (
+    data: string,
+    onUploadProgress?: (event: ProgressEvent) => void
+  ) => Promise<AxiosResponse<void>>
 }
 
 export default function (instance: AxiosInstance): INetworkAxios {
@@ -18,17 +22,26 @@ export default function (instance: AxiosInstance): INetworkAxios {
       return instance.get('tools/ip')
     },
     checkPing(): Promise<AxiosResponse<void>> {
-      return instance.get('tools/ping')
+      return instance.get('tools/ping', { headers: { 'Cache-Control': 'no-cache' } })
     },
-    getRX(onDownloadProgress): Promise<AxiosResponse<void>> {
-      return instance.get('tools/rx', {
-        onDownloadProgress: onDownloadProgress
+    getRX(onUploadProgress): Promise<AxiosResponse<void>> {
+      return instance.post(
+        'tools/rx',
+        {},
+        {
+          onUploadProgress: onUploadProgress,
+          headers: { 'Cache-Control': 'no-cache' }
+        }
+      )
+    },
+    getTX(data, onUploadProgress): Promise<AxiosResponse<void>> {
+      return instance.post('tools/tx', data, {
+        onUploadProgress: onUploadProgress,
+        headers: {
+          'Content-Type': 'application/octet-stream',
+          'Cache-Control': 'no-cache'
+        }
       })
-    },
-    getTX(onDownloadProgress): Promise<AxiosResponse<void>> {
-      return instance.get('tools/tx', {
-        onDownloadProgress: onDownloadProgress
-      })
-    },
+    }
   }
 }
--- src/components/admin/modals/SettingsView/NetworkSettings/NetworkSettings.tsx
+++ src/components/admin/modals/SettingsView/NetworkSettings/NetworkSettings.tsx
@@ -1,37 +1,99 @@
-import React, {FC, useCallback, useEffect, useState} from 'react'
-import {Text} from '@consta/uikit/Text'
-import {ProgressLine} from '@consta/uikit/ProgressLine'
+import React, { Dispatch, FC, useCallback, useEffect, useMemo, useState } from 'react'
+import { Text } from '@consta/uikit/Text'
+import { ProgressLine } from '@consta/uikit/ProgressLine'
 import cl from './NetworkSettings.module.scss'
-import {Button} from '@consta/uikit/Button'
-import {IconPause} from '@consta/uikit/IconPause'
-import {IconPlay} from '@consta/uikit/IconPlay'
-import {IpInfo} from '../../../../../api/axios/modules/network';
-import {request} from '../../../../../api/axios/request';
-import dayjs, {Dayjs} from 'dayjs';
+import { Button } from '@consta/uikit/Button'
+import { IconPause } from '@consta/uikit/IconPause'
+import { IconPlay } from '@consta/uikit/IconPlay'
+import { IpInfo } from '../../../../../api/axios/modules/network'
+import { request } from '../../../../../api/axios/request'
+import dayjs, { Dayjs } from 'dayjs'
+import { getColumnLeftOffset } from '@consta/uikit/__internal__/src/components/Table/helpers'
 
 const NetworkSettings: FC = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const [ipInfo, setIpInfo] = useState<IpInfo | undefined>(undefined)
   const [ping, setPing] = useState<number | undefined>(undefined)
+
+  const [rx, setRx] = useState<number | undefined>(undefined)
+  const [tx, setTx] = useState<number | undefined>(undefined)
+
+  const [rxProgress, setRxProgress] = useState<number | undefined>(undefined)
+  const [txProgress, setTxProgress] = useState<number | undefined>(undefined)
 
   const resetData = useCallback<() => void>(() => {
     setIpInfo(undefined)
     setPing(undefined)
+    setRx(undefined)
+    setTx(undefined)
+    setRxProgress(undefined)
+    setTxProgress(undefined)
   }, [])
 
-  const getIpInfo = useCallback<() => Promise<void>>(async () => {
-    await request.network.getIpInfo().then(r => setIpInfo(r.data))
-  }, [])
-
-  const getPing = useCallback<() => Promise<void>>(async () => {
-    const currentTime: number = dayjs().millisecond()
-    await request.network.checkPing().then(() => setPing(-currentTime + dayjs().millisecond()))
+  const buffer = useMemo<string>(() => {
+    let res = 'x'
+    for (let i = 0; i < 23; i++) {
+      res += res
+    }
+    return res
   }, [])
 
   useEffect(() => {
+    const getIpInfo = async () => {
+      const res = await request.network.getIpInfo()
+      setIpInfo(res.data)
+    }
+
+    const getPing = async () => {
+      const currentTime: number = dayjs().unix()
+      console.log(currentTime)
+
+      await request.network.checkPing().then(() => console.log(dayjs().unix()))
+
+      console.log(dayjs().unix())
+      const currentPing = dayjs().unix() - currentTime
+      setPing(currentPing)
+    }
+
+    const setResOrProgress = (
+      setProgress: Dispatch<number | undefined>,
+      setRes: Dispatch<number | undefined>,
+      progress: ProgressEvent,
+      previousTime: number
+    ) => {
+      const currentProgress = Math.floor((progress.loaded / progress.total) * 100)
+
+      if (currentProgress < 100) {
+        setProgress(currentProgress)
+      } else {
+        const currentPing = ping ?? 0
+        const diff = dayjs().unix() - previousTime - currentPing || 1
+        setRes(Number(((8 * 1000 * 8) / diff).toFixed(2)))
+        setProgress(undefined)
+      }
+    }
+
+    const checkTX = async () => {
+      const currentTime = dayjs().unix()
+
+      await request.network.getTX(buffer, (progress) => {
+        setResOrProgress(setTxProgress, setTx, progress, currentTime)
+      })
+    }
+
+    const checkRX = async () => {
+      const currentTime = dayjs().unix()
+
+      await request.network.getRX((progress) => {
+        setResOrProgress(setRxProgress, setRx, progress, currentTime)
+      })
+    }
+
     if (isLoading) {
       resetData()
-      getIpInfo().then(() => getPing().then(() => setIsLoading(false)))
+      getIpInfo().then(() =>
+        getPing().then(() => checkTX().then(() => checkRX().then(() => setIsLoading(false))))
+      )
     }
   }, [isLoading])
 
@@ -63,22 +125,46 @@ const NetworkSettings: FC = () => {
             {ipInfo ? `${ipInfo.country}, ${ipInfo.city}` : '-'}
           </Text>
           <Text view={'primary'} size={'s'}>
-            {ping ? `${ping} мс` : '-'}
+            {ping !== undefined ? `${ping} мс` : '-'}
           </Text>
-          {/* <Text view={'primary'} size={'s'}> */}
-          {/*   32.55 Мбит/с */}
-          {/* </Text> */}
-          <ProgressLine className={cl.progress}/>
-          <ProgressLine className={cl.progress}/>
+          {txProgress ? (
+            <ProgressLine value={txProgress} className={cl.progress} />
+          ) : (
+            <Text view={'primary'} size={'s'}>
+              {tx !== undefined ? `${tx} Мбит/с` : '-'}
+            </Text>
+          )}
+
+          {rxProgress ? (
+            <ProgressLine value={rxProgress} className={cl.progress} />
+          ) : (
+            <Text view={'primary'} size={'s'}>
+              {rx !== undefined ? `${rx} Мбит/с` : '-'}
+            </Text>
+          )}
         </div>
       </div>
       <div className={cl.buttonWrapper}>
-        {
-          !isLoading ? <Button iconLeft={IconPlay} size={'s'} view={'primary'} label={'Проверить'}
-                               onClick={() => setIsLoading(true)}/> :
-            <Button iconLeft={IconPause} size={'s'} view={'primary'} label={'Остановить'}
-                    onClick={() => setIsLoading(false)}/>
-        }
+        {!isLoading ? (
+          <Button
+            iconLeft={IconPlay}
+            size={'s'}
+            view={'primary'}
+            label={'Проверить'}
+            onClick={() => setIsLoading(true)}
+          />
+        ) : (
+          <Button
+            iconLeft={IconPause}
+            size={'s'}
+            view={'primary'}
+            label={'Остановить'}
+            onClick={() => {
+              setIsLoading(false)
+              resetData()
+            }}
+          />
+        )}
       </div>
     </div>
   )
